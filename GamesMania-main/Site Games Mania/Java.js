812deb5ba13988f6a6e4a99bc5a6fe20546bf741
DustIago <90655777+DustIago@users.noreply.github.com>
2021-12-14 17:06:28 -0300

    Delete GamesMania-main directory

--- GamesMania-main/Site Games Mania/Java.js
+++
@@ -1,9 +0,0 @@
-mobile.onclik = function(){
-    for(i=0;i<menu.lenght;i++){
-        if(menu[i].style.display == 'inline'){
-            menu[i].style.display = 'none'
-        } else{
-            menu[i].style.display = 'inline'
-        }
-    }
-}
